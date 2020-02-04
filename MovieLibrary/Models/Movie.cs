4135fb69b3ad0e23efd10c8dba39d66ae90f2bca
Kelvin Quinonez <kesqui7@gmail.com>
2020-02-03 22:39:42 -0600

    Updated Movie model with more props for movie details.

--- MovieLibrary/Models/Movie.cs
+++ MovieLibrary/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,5 +10,10 @@ namespace MovieLibrary.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? ReleaseDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? DateAdded { get; set; }
+        public int NumberInStock { get; set; } 
     }
 }
