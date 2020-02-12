010e3abec5ffb994285081f6a0bbc57fec3f5851
Kelvin Quinonez <kesqui7@gmail.com>
2020-02-11 20:01:55 -0600

    Final edits for editing a customer profile.

--- MovieLibrary/Controllers/CustomersController.cs
+++ MovieLibrary/Controllers/CustomersController.cs
@@ -54,7 +54,17 @@ namespace MovieLibrary.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
