24ed7b2fadfde702cb69cb54dda28bba8b9ae954
Kelvin Quinonez <kesqui7@gmail.com>
2020-02-12 23:02:24 -0600

    Finalizing front-end form for adding a new movie.

--- MovieLibrary/Models/Movie.cs
+++ MovieLibrary/Models/Movie.cs
@@ -9,13 +9,20 @@ namespace MovieLibrary.Models
     public class Movie
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         [DataType(DataType.Date)]
         public DateTime? ReleaseDate { get; set; }
+
         [DataType(DataType.Date)]
         public DateTime? DateAdded { get; set; }
+
+        [Display(Name = "Number In Stock")]
         public int NumberInStock { get; set; }
+
         public Genre Genre { get; set; }
+
         public byte GenreId { get; set; }
     }
 }
