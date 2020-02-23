6c6cfcd64ec71f9f1bd4820fceae5aad0946dd99
Kelvin Quinonez <kesqui7@gmail.com>
2020-02-23 15:19:13 -0600

    Fixed client id error from displaying when form is not completed for a new customer.

--- MovieLibrary/Controllers/CustomersController.cs
+++ MovieLibrary/Controllers/CustomersController.cs
@@ -45,6 +45,7 @@ namespace MovieLibrary.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
             
